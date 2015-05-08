f00a265c54a5a5863b6ef6c7a663224b67a158c9
Dieter Plaetinck <dieter@plaetinck.be>
2015-05-08 13:26:59 -0400

    remove old pyblosxom stuff

--- plugins/guid.py
+++
@@ -1,6 +0,0 @@
-# taken from documentation, written by Will
-# add 'guid' property to entry to override the GUID
-def cb_story(args):
-	entry = args["entry"]
-	entry["guid"] = entry.get("guid", entry.get("file_path"))
-	return args

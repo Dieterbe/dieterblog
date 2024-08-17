17acf113dcfdd3b2408b0951dbf1e7a3273d4ba9
Dieter Plaetinck <dieter@plaetinck.be>
2024-08-17 16:19:57 +0300

    dev needs drafts

--- deploy-dieter-dev.sh
+++ deploy-dieter-dev.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo ">>>>> build"
 rm -rf pub/public-dieter-dev
-hugo --config config-dieter-dev.toml -d pub/public-dieter-dev
+hugo --config config-dieter-dev.toml --buildDrafts -d pub/public-dieter-dev
 rsync -a --delete files/ pub/public-dieter-dev/files/
 echo ">>>>> deploy"
 rsync -a --delete pub/public-dieter-dev/ arch1:/srv/http/dieter-dev/

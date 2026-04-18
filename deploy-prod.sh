ab94a60c66ddd3ff1a60eeda0c7db2cf2173abc7
Dieter Plaetinck <dieter@plaetinck.be>
2026-04-18 11:55:19 +0200

    version prod deploys, track all of them

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo ">>>>> build"
-rm -rf pub/public-prod
-hugo --config config-prod.toml -d pub/public-prod
-rsync -a --delete files/ pub/public-prod/files/
+d=pub/public-prod-$(date +'%Y-%m-%d_%H-%M-%S')
+echo "dir: $d"
+hugo --config config-prod.toml -d $d
+rsync -a --delete files/ $d/files/
 echo ">>>>> deploy"
-rsync -a --delete pub/public-prod/ arch1:/srv/http/dieter/
+rsync -a --delete $d/ arch1:/srv/http/dieter/
